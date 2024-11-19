5c4338887a4d471f9291ec401c38854119a74047
Belen Chiarino <beluchiarino@hotmail.com>
2024-11-19 16:45:53 +0100

    finished part 1

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :pet
   belongs_to :user
+
+  validates :booking_date, presence: true
+  validates :booking_location, presence: true
+
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,9 @@
 class Pet < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :pet_details, presence: true
+  validates :pet_name, presence: true
+  validates :picture_url, presence: true
   # has_many :users, through: :bookings
 end
