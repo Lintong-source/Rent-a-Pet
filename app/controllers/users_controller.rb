feddadf96076f94631f244d9da4615641931e5ac
Imer Durgut <idurgut@gmail.com>
2024-11-20 15:49:06 +0100

    login redirecred to homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
- def profile
-  params[:username]
- end
+  def profile
+    params[:username]
+  end
+
+  def destroy
+    sign_out(current_user)
+      redirect_to root_path, notice: "You have successfully logged out."
+  end
 end
