c9bfb2a6d756efd80009b4041a249e6cd235cd08
Lintong Jin <Lt.jin@hotmail.com>
2024-11-20 14:48:34 +0100

    add a map on the home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,30 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-user = User.create!(user_name: "belumental", email: "belumental@email.com", password: "topsecret", password_confirmation: "topsecret")
+# user = User.create!(user_name: "Lintong", email: "lt.jn@hotmail.com ", password: "topsecret", password_confirmation: "topsecret")
 
-Pet.create(pet_name: "Cookie", pet_details: "Lovely french bulldog, very friendly", picture_url: "https://i.guim.co.uk/img/media/38b9433aa08dc7ebeaf74405b24f19f7be0078c7/0_100_3000_1801/master/3000.jpg?width=620&dpr=2&s=none&crop=none", user_id: user.id)
-
-
-Pet.create(pet_name: "Peanut Butter", pet_details: "Loves to go for walks in the forest", picture_url: "https://image.petmd.com/files/styles/978x550/public/2023-01/toy-poodle.jpg", user_id: user.id)
+# Pet.create(pet_name: "Cookie", pet_details: "Lovely french bulldog, very friendly", picture_url: "https://i.guim.co.uk/img/media/38b9433aa08dc7ebeaf74405b24f19f7be0078c7/0_100_3000_1801/master/3000.jpg?width=620&dpr=2&s=none&crop=none", user_id: user.id)
 
 
-Pet.create(pet_name: "Shrek", pet_details: "Cuddly and active little dog, loves to play ball", picture_url: "https://hondenpassie.nl/wp-content/uploads/2021/05/engelse-cocker-spanie%CC%88l-ligt-op-bed.jpeg", user_id: user.id)
+# Pet.create(pet_name: "Peanut Butter", pet_details: "Loves to go for walks in the forest", picture_url: "https://image.petmd.com/files/styles/978x550/public/2023-01/toy-poodle.jpg", user_id: user.id)
 
 
-Pet.create(pet_name: "Wilfred", pet_details: "Take Wilfred for a run, he is very active and friendly", picture_url: "https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=532,height=532,fit=cover/animal/breed/dog/adult/66fc1c266f250851700106.jpg", user_id: user.id)
+# Pet.create(pet_name: "Shrek", pet_details: "Cuddly and active little dog, loves to play ball", picture_url: "https://hondenpassie.nl/wp-content/uploads/2021/05/engelse-cocker-spanie%CC%88l-ligt-op-bed.jpeg", user_id: user.id)
 
 
-Pet.create(pet_name: "Snowball", pet_details: "Have a beautiful day next to Snowball", picture_url: "https://www.meleklermekani.com/imagehosting/314434dbe7abc63b39.jpg", user_id: user.id)
-
-Pet.create(pet_name: "Princess", pet_details: "Princess enjoys fresh air and nature time", picture_url: "https://www.elityavru.com/images/irk-bilgileri/kopek-irki/shih-tzu-1.webp", user_id: user.id)
+# Pet.create(pet_name: "Wilfred", pet_details: "Take Wilfred for a run, he is very active and friendly", picture_url: "https://cdn.wamiz.fr/cdn-cgi/image/format=auto,quality=80,width=532,height=532,fit=cover/animal/breed/dog/adult/66fc1c266f250851700106.jpg", user_id: user.id)
 
 
-Pet.create(pet_name: "Boris", pet_details: "Big cheerful dog, knows tricks", picture_url: "https://images.ctfassets.net/b85ozb2q358o/5aOPXNiyHE5jb7ZYoN7rHw/2a0312496041361da7470b5ac9113b91/bouvier_bernois_1_363880969.jpeg", user_id: user.id)
+# Pet.create(pet_name: "Snowball", pet_details: "Have a beautiful day next to Snowball", picture_url: "https://www.meleklermekani.com/imagehosting/314434dbe7abc63b39.jpg", user_id: user.id)
+
+# Pet.create(pet_name: "Princess", pet_details: "Princess enjoys fresh air and nature time", picture_url: "https://www.elityavru.com/images/irk-bilgileri/kopek-irki/shih-tzu-1.webp", user_id: user.id)
+
+
+# Pet.create(pet_name: "Boris", pet_details: "Big cheerful dog, knows tricks", picture_url: "https://images.ctfassets.net/b85ozb2q358o/5aOPXNiyHE5jb7ZYoN7rHw/2a0312496041361da7470b5ac9113b91/bouvier_bernois_1_363880969.jpeg", user_id: user.id)
+
+puts('seed a new pet with address')
+#Flat.create(address: "16 Villa Gaudelet, Paris", name: "Le Wagon HQ")
+
+Pet.create(pet_name:"Ace", pet_details: "Have fun with Ace!", picture_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.atozvet.com%2Fprevention-treatment-pet-disease%2F&psig=AOvVaw3iPhWgbHr3vX7KCvIELP1W&ust=1732184928734000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJiD2u3Z6okDFQAAAAAdAAAAABAE", address: "16 Villa Gaudelet, Paris", user_id: 1)
+
+puts('done')
